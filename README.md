af1a2d3caf11d400523ad916a7e5241852b3b62a
Andrew Yan <andrewyan@andrews-mbp.attlocal.net>
2018-08-04 18:11:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Script to generate random combination of taekwando kicks.
+
+This script can be run either using Python 2 or 3. The script
+will default to generating a combination of three kicks.
+
+Example:
+
+```shell
+python kicks.py  # generate combination of three
+python kicks.py --length 9  # generate a combination of nine
+```
---
+++ kicks.py
@@ -0,0 +1,51 @@
+import argparse
+import random
+
+
+KICKS = (
+    'front',
+    'jump front',
+    'skip front',
+    'flying front',
+    'roundhouse',
+    'jump roundhouse',
+    'skip roundhouse',
+    'flying roundhouse',
+    'spin flying roundhouse',
+    'side',
+    'step side',
+    'flying side',
+    'hook',
+    'spin hook',
+    'jump spin hook',
+    'flying spin hook',
+    'back',
+    'jump back',
+    'flying back',
+    'inside-out crescent',
+    'outside-in crescent',
+    'flying crescent',
+    'spin crescent',
+)
+
+
+def generate_random_combo(combo_length=3):
+    combo = []
+    while len(combo) < combo_length:
+        combo.append(random.choice(KICKS))
+    return ', '.join(combo)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--length',
+        type=int,
+        default=3,
+        help='Length of the combination to generate.'
+    )
+    args = parser.parse_args()
+    combo_length = args.length
+
+    result = generate_random_combo(combo_length)
+    print(result)
